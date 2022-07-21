036c4ae34ccfec36347f7af8f4504a2942608813
enessaid2701 <a.saidenes@gmail.com>
2022-07-21 17:04:26 +0300

    fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@Configuration
+@PropertySource("classpath:system.properties")
 public class DemoApplication {
 
     public static void main(String[] args) {
